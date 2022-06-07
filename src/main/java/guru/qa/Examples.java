89fbf2a7304dd83f3f2cceef2998e1d11faedaa9
Daniil Glushenkov <glushenkov.daniel@yandex.ru>
2022-06-08 01:12:21 +0300

    added class Examples

---
+++ src/main/java/guru/qa/Examples.java
@@ -0,0 +1,30 @@
+package guru.qa;
+
+public class Examples {
+    public static void main(String[] args) {
+        int a = 1000/2;
+        int q = 100*3;
+        System.out.println("example: " + (a - q + 100));
+
+ /*       byte b = 50 - 2;
+        long l = 50000;
+        short s = 700;
+        double d = 56.6d;
+        float f = 10.5f;
+        char c = 'A';
+        boolean bol = a == b;
+        boolean bol2 = b < l;
+        boolean bol3 = (s * 20) < l;
+
+        System.out.println("example of byte " + "= " + (b * 2) / 4);
+        System.out.println("example of long " + "= " + (l / 10 + 100) * 2);
+        System.out.println("example of short " + "= " + (s % 15 * s) / 5);
+        System.out.println("example of double " + "= " + (d - 22.22) * 0.5);
+        System.out.println("example of char " + c + (c * 2));
+        System.out.println("example of mixed types " + "= " + a % b + l / 2 + (d - f) + c);
+        System.out.println(bol);
+        System.out.println(bol2);
+        System.out.println(bol3);
+
+  */
+}

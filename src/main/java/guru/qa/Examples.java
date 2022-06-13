f776546f38bbff9c7d2060fec0b75056cb9a06c2
Daniil Glushenkov <glushenkov.daniel@yandex.ru>
2022-06-13 15:19:36 +0300

    added examples

--- src/main/java/guru/qa/Examples.java
+++ src/main/java/guru/qa/Examples.java
@@ -2,29 +2,53 @@ package guru.qa;
 
 public class Examples {
     public static void main(String[] args) {
-        int a = 1000/2;
-        int q = 100*3;
-        System.out.println("example: " + (a - q + 100));
+//      String — строковые переменные, не представляют собой примитивы и хранят в себе целые строки.
+        String name = "Daniil";
+        System.out.println("example of String: " + "Привет, " + name);
 
- /*       byte b = 50 - 2;
-        long l = 50000;
-        short s = 700;
-        double d = 56.6d;
-        float f = 10.5f;
-        char c = 'A';
-        boolean bol = a == b;
-        boolean bol2 = b < l;
-        boolean bol3 = (s * 20) < l;
+//      char — хранит одиночный символ и занимает 2 байта памяти.
+        char k = 'K';
+        System.out.println("example of char: " + k + "ot-Kotochek");
 
-        System.out.println("example of byte " + "= " + (b * 2) / 4);
-        System.out.println("example of long " + "= " + (l / 10 + 100) * 2);
-        System.out.println("example of short " + "= " + (s % 15 * s) / 5);
-        System.out.println("example of double " + "= " + (d - 22.22) * 0.5);
-        System.out.println("example of char " + c + (c * 2));
-        System.out.println("example of mixed types " + "= " + a % b + l / 2 + (d - f) + c);
-        System.out.println(bol);
-        System.out.println(bol2);
-        System.out.println(bol3);
+//      int — целочисленный тип, хранит целое число от -2147483648 до 2147483647 и занимает 4 байта памяти.
+        int a = 1000 / 2;
+        int b = 100 * 3;
+        System.out.println("example of int: " + (a - b + 22));
 
-  */
+//      byte — целочисленный тип, хранит целое число от -128 до 127 и занимает 1 байт памяти.
+        byte c = -128;
+        byte d = 127;
+        System.out.println("example of byte: " + (c + d + 6) % 2);
+
+//      short — целочисленный тип, хранит целое число от -32768 до 32767 и занимает 2 байта памяти.
+        short e = -29998;
+        e--;
+        e--;
+        System.out.println("example of short: " + (-e) * (-1));
+
+/*      long — целочисленный тип, хранит целое число от –9 223 372 036 854 775 808 до 9 223 372 036 854 775 807
+      и занимает 8 байт памяти. */
+        long f = 1234567890123456L;
+        f++;
+        System.out.println("example of long: " + f % 1000000);
+
+//      float — хранит число с плавающей точкой (до 7 десятичных цифр)
+        float g = 7.12345678F;
+        float h = 0;
+        h += g;
+        System.out.println("example of float: " + h);
+
+//      double — хранит число с плавающей точкой (до 15 десятичных цифр)
+        double j = 133.9476762553;
+        System.out.println("example of double: " + j / 2 * (-1.5));
+
+//      boolean — логические тип данных, хранит в себе значение true и false.
+        boolean ex1 = a == b;
+        boolean ex2 = e >= f;
+        boolean ex3 = h != j;
+        System.out.println("example of boolean: " + ex1);
+        System.out.println("example of boolean: " + ex2);
+        System.out.println("example of boolean: " + ex3);
+
+    }
 }
